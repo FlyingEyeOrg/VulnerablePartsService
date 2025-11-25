5e5d269f8b0d35e01fb8f45a6f8ec0f3c0c4df61
lanxiaofang <13604461032@163.com>
2025-11-25 20:07:05 +0800

    添加发布脚本。

---
+++ src/FlyingEye.VulnerablePartsService/public.bat
@@ -0,0 +1,23 @@
+﻿@echo off
+:: 设置控制台编码为UTF-8
+chcp 65001 > nul
+setlocal enabledelayedexpansion
+
+:: 1. 基础配置
+set "APP_NAME=VulnerablePartsService-v1.0.0"
+set "SCRIPT_DIR=%~dp0"
+set "CURRENT_PROJECT=%SCRIPT_DIR%..\FlyingEye.VulnerablePartsService\FlyingEye.VulnerablePartsService.csproj"
+set "PUBLISH_ROOT=%SCRIPT_DIR%bin\publish\%APP_NAME%"
+set "CONFIGURATION=Release"
+set "RUNTIME=linux-x64"
+
+echo.
+echo 正在发布主程序...
+dotnet publish "%CURRENT_PROJECT%" -c %CONFIGURATION% -r %RUNTIME% -o "%PUBLISH_ROOT%" --no-self-contained
+
+echo.
+echo 发布完成
+start "" explorer "%PUBLISH_ROOT%"
+
+pause
+exit /b 0
